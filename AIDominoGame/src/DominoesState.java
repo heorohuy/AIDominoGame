3adc2a48d6c6237ecb5364237cad68e9f37464b0
heorohuy <gustavo.serrano@upr.edu>
2014-03-02 22:53:53 -0400

    PieceTracker completed

--- AIDominoGame/src/DominoesState.java
+++ AIDominoGame/src/DominoesState.java
@@ -4,28 +4,33 @@ import java.util.ArrayList;
 
 
 public class DominoesState {
-	
+
 	private Pair endPoints;
 	private ArrayList<Pair> playedTiles = new ArrayList<Pair>();
 	private ArrayList<ArrayList<Pair>> playerHands = new ArrayList<ArrayList<Pair>>();
 	private int currentTurn = 0;
 	private int passes = 0;
+	private ArrayList<PieceTracker> trackers = new ArrayList<PieceTracker>(); 
 
 	public DominoesState(Pair endPoints, ArrayList<Pair> playedTiles,
 			ArrayList<ArrayList<Pair>> playerHands, int currentTurn, 
 			int passes){
-		
+
 		this.endPoints = endPoints;
 		this.currentTurn = currentTurn;
 		this.passes = passes;
 		this.playedTiles = playedTiles;
 		this.playerHands = playerHands;
 	}
-	
+
+	public ArrayList<Pair> getHand(int player){
+		return playerHands.get(player);
+	}
+
 	public Pair getEndPoints(){
 		return endPoints;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public DominoesState makeMove(MoveData move){
 		ArrayList<ArrayList<Pair>> nextPlayerHands = new ArrayList<ArrayList<Pair>>();
@@ -33,29 +38,35 @@ public class DominoesState {
 		Pair nextEndPoints;
 		if(isMoveLegal(move)){
 			if(move.getEndPoint() == -1){
+				for(int i = 0; i < 4; i++)
+					if(i != currentTurn)
+						trackers.get(i).playerPassed();
 				return new DominoesState(endPoints, playedTiles, 
 						playerHands, currentTurn+1, passes+1);
 			}
-			
+
 			if(move.getEndPoint() == endPoints.getX()){
 				nextEndPoints = new Pair(endPoints.getY(),move.getTile().getOtherSide(move.getEndPoint()));
 			}else{
 				nextEndPoints = new Pair(endPoints.getX(),move.getTile().getOtherSide(move.getEndPoint()));
 			}
-			
+
 			nextPlayedTiles = (ArrayList<Pair>)playedTiles.clone();
 			nextPlayedTiles.add(move.getTile());
-			
+
 			nextPlayerHands = (ArrayList<ArrayList<Pair>>)playerHands.clone();;
 			nextPlayerHands.set(currentTurn, (ArrayList<Pair>)(playerHands.get(currentTurn).clone()));
 			nextPlayerHands.get(currentTurn).remove(move.getTile());
-			
+
+			for(int i = 0; i < 4; i++)
+				trackers.get(i).tilePlayed(move.getTile());
+
 			return new DominoesState(nextEndPoints, nextPlayedTiles,
 					nextPlayerHands, (currentTurn + 1)&3, 0);
 		}
 		throw new IllegalArgumentException("Not legal move.");
 	}
-	
+
 	public boolean isMoveLegal(MoveData move){
 		ArrayList<MoveData> legalMoves = getLegalMoves();
 		if(move.getEndPoint() == -1){
@@ -64,11 +75,11 @@ public class DominoesState {
 			return legalMoves.contains(move);
 		}
 	}
-	
+
 	public ArrayList<MoveData> getLegalMoves(){
-		
+
 		ArrayList<MoveData> legalMoves = new ArrayList<MoveData>();
-		
+
 		for(Pair tile : playerHands.get(currentTurn)){
 			if(tile.canConnect(endPoints.getX())){
 				legalMoves.add(new MoveData(tile, endPoints.getX()));
@@ -80,16 +91,16 @@ public class DominoesState {
 		}
 		return legalMoves;
 	}
-	
+
 	public int getCurrentTurn(){
 		return currentTurn;
 	}
-	
+
 	public boolean hasGameEnded(){
 		for(ArrayList<Pair> hand : playerHands)
 			if(hand.isEmpty())
 				return true;
-		
+
 		return passes >= 4;
 	}
 }
--- AIDominoGame/src/PieceTracker.java
+++ AIDominoGame/src/PieceTracker.java
@@ -2,17 +2,47 @@ import java.util.ArrayList;
 
 
 public class PieceTracker {
-	
-	private ArrayList<Pair> candidates = new ArrayList<Pair>();
-	
-	public PieceTracker(DominoesState state){
-		//TODO
+
+	private ArrayList<ArrayList<Pair>> candidates = new ArrayList<ArrayList<Pair>>();
+	private DominoesState state;
+	private int myTurn;
+
+	public PieceTracker(DominoesState state, int myTurn){
+		this.state = state;
+		this.myTurn = myTurn;
+
 		for(int i = 0; i < 7; i++)
 			for(int j = 0; j < 7; j++)
-				candidates.add(new Pair(i,j));
-		
+				for(int k = 0; k < 4 ; k++)
+					if(k != myTurn && !candidates.get(k).contains(new Pair(i,j)))
+							candidates.get(k).add(new Pair(i,j));
+		this.removeHand();
+	}
+
+	private void removeHand(){
+		for(Pair tile : state.getHand(myTurn)){
+			for(int i = 0; i < 4; i++)
+				if(i != myTurn && candidates.get(i).contains(tile))
+						candidates.get(i).remove(tile);
+		}
 	}
 	
+	public void tilePlayed(Pair tile){
+		for(int i = 0; i < 4; i++)
+			if(i != myTurn)
+				candidates.get(i).remove(tile);
+	}
+
+	public void playerPassed(){
+		for(Pair tile : candidates.get(state.getCurrentTurn())){
+			if(tile.canConnect(state.getEndPoints().getX()) || tile.canConnect(state.getEndPoints().getY())){
+				candidates.get(state.getCurrentTurn()).remove(tile);
+			}
+		}
+	}
 	
+	public ArrayList<Pair> getPossiblePlayerHand(int player){
+		return candidates.get(player);
+	}
 
 }
